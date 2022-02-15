96df2807a93afa9efa5c7560b8d87cc703f2352b
Emil <emiljanQ3@hotmail.com>
2022-02-15 23:11:54 +0100

    UPDATE config for verfying genetic training

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from numpy import pi
 class Params:
     def __init__(self):
         # Meta
-        self.num_steps = 10000
-        self.num_repeats = 1
-        self.num_agents = 100
-        self.save_id = "gentic_run_3-"
-        self.results_path = "results/genetic1/"
+        self.num_steps = 100000
+        self.num_repeats = 100
+        self.num_agents = 10
+        self.save_id = "validation-"
+        self.results_path = "results/genetic_validation1/"
         self.alpha_tag = AlphaInitTag.NETWORK
         # Learning
-        self.is_genetic_training = True
+        self.is_genetic_training = False
         self.is_backprop_training = False
         self.update_memory = True
         self.memory_length = 16
@@ -30,9 +30,9 @@ class Params:
         self.world_width = 2
         # Record
         self.is_recording_position = False
-        self.is_recording_area = True
+        self.is_recording_area = False
         self.is_recording_area_indices = False
-        self.is_recording_area_over_time = False
+        self.is_recording_area_over_time = True
         self.is_recording_loss = False
         self.is_recording_buffer_dataset = False
         self.area_unit_size = 0.05
