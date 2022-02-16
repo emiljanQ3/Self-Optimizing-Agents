62aba7e4b8447779e83247949ef6ee44930488dc
Emil <emiljanQ3@hotmail.com>
2022-02-16 11:09:53 +0100

    UPDATE config for data inspection

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Params:
     def __init__(self):
         # Meta
         self.num_steps = 100000
-        self.num_repeats = 100
-        self.num_agents = 10
-        self.save_id = "validation-"
-        self.results_path = "results/genetic_validation1/"
-        self.alpha_tag = AlphaInitTag.NETWORK
+        self.num_repeats = 1
+        self.num_agents = 11
+        self.save_id = "run_linspace"
+        self.results_path = "results/inspecting_data/"
+        self.alpha_tag = AlphaInitTag.LINSPACE
         self.model_location = "saved_genetic_training/gentic_run_3-/gentic_run_3-"
         # Learning
         self.is_genetic_training = False
@@ -21,7 +21,7 @@ class Params:
         self.delta_time = 0.5
         self.speed = 0.01
         self.ang_sd = pi/6
-        self.selected_mover = MoveTag.NEURAL_LEVY
+        self.selected_mover = MoveTag.LEVY_VARYING_DELTA_CONTRAST
         self.alpha = 1
         # World
         self.selected_world = WorldTag.CONCAVE_CELLS
@@ -30,7 +30,7 @@ class Params:
         self.world_height = 2
         self.world_width = 2
         # Record
-        self.is_recording_position = False
+        self.is_recording_position = True
         self.is_recording_area = False
         self.is_recording_area_indices = False
         self.is_recording_area_over_time = True
