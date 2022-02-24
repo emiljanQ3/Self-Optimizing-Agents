ee82a57a9c6fbc0b61ca65575c48f1a3a04d2532
Emil <emiljanQ3@hotmail.com>
2022-02-24 11:19:49 +0100

    ADD tqdm bars to simulation.py

--- simulation.py
+++ simulation.py
@@ -1,11 +1,12 @@
 from init import init_agents_data, init_agents_pos
+from tqdm import tqdm
 
 
 def simulate(world, mover, data_recorder, data_modifier, params, models=None):
-    for rep in range(params.num_repeats):
+    for rep in tqdm(range(params.num_repeats), leave=False):
         agents_pos = init_agents_pos(params)
         agents_data = init_agents_data(params, models)
-        for step in range(params.num_steps):
+        for step in tqdm(range(params.num_steps), leave=False):
             new_agents_pos, agents_data = mover.step(agents_pos, agents_data, params)
             new_agents_pos = world.step(agents_pos, new_agents_pos, params)
             data_recorder.record(agents_pos, new_agents_pos, agents_data, world, mover, rep, step)
