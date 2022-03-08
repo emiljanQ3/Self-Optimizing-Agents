620b61cd881a0b8c26bffb4b7053f29370154018
Emil <emiljanQ3@hotmail.com>
2022-03-08 14:49:20 +0100

    UPDATE move to optimize for multiple local alphas

--- move.py
+++ move.py
@@ -5,6 +5,9 @@ from scipy.stats import levy_stable
 from tags import MoveTag, AlphaInitTag
 from config import Params
 
+optimal_alphas = {6: 1.0, 5: 1.0, 4: 1.0, 3: 1.0, 2: 1.1, 1: 1.2, 0: 1.2,
+                               -1: 1.5, -2: 2.0, -3: 1.9, -4: 2.0, -5: 2.0, -6: 2.0}
+
 
 class Mover:
     def __init__(self, components):
@@ -97,15 +100,13 @@ class LevyRotaterVaryingDelta:
 class AlwaysOptimalLevyRotater:
     def __init__(self, params):
         self.levy_timer = np.zeros(params.num_agents)
-        self.optimal_alphas = {6: 1.0, 5: 1.0, 4: 1.0, 3: 1.0, 2: 1.1, 1: 1.2, 0: 1.2,
-                               -1: 1.5, -2: 2.0, -3: 1.9, -4: 2.0, -5: 2.0, -6: 2.0}
 
     def apply(self, agents, agents_data, params):
         self.levy_timer -= params.delta_time * delta_variation(x=agents[:, 0], width=params.world_width)
         turning_idx = np.argwhere(self.levy_timer <= 0)
         for i in turning_idx:
             self.levy_timer[i] = np.abs(levy_stable.rvs(
-                alpha=self.optimal_alphas[cell_exponent(agents[i, 0].item(), params.world_width)], beta=0))
+                alpha=optimal_alphas[cell_exponent(agents[i, 0].item(), params.world_width)], beta=0))
             agents[i, 2] += np.random.standard_normal() * params.ang_sd
 
         return agents, agents_data
@@ -131,14 +132,13 @@ class BigContrastOptimalLevyRotater:
         self.levy_timer = np.zeros(params.num_agents)
         self.instant_switch = instant_switch
         self.last_x = 0
-        # self.optimal_alphas = {3: 1.0, -4: 2.0}
 
     def apply(self, agents, agents_data, params):
         self.levy_timer -= params.delta_time * big_contrast_delta_variation(x=agents[:, 0], params=params)
         turning_idx = np.argwhere(np.logical_or(self.levy_timer <= 0,  self.apply_instant_switch(agents)))
         for i in turning_idx:
             self.levy_timer[i] = np.abs(levy_stable.rvs(
-                alpha=math.floor(agents[i, 0].item()) % 2 + 1, beta=0))
+                alpha=optimal_alphas[params.tic_rate_0] if math.floor(agents[i, 0].item()) % 2 == 0 else optimal_alphas[params.tic_rate_1], beta=0))
             agents[i, 2] += np.random.standard_normal() * params.ang_sd
 
         return agents, agents_data
