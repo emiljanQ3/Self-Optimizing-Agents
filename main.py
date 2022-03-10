00fbc07c6acd54545b91320c2dd4cb43e7301985
Emil <emiljanQ3@hotmail.com>
2022-03-10 15:06:35 +0100

    UPDATE config.py for genetic_run_2_-6 validation

--- main.py
+++ main.py
@@ -72,8 +72,8 @@ def run_paralell(params: Params, num_simulations):
 if __name__ == '__main__':
     params = Params()
     # run_paralell(params, 8)
-    run_contrast_param_search(params)
-    #run_simulation(params)
+    # run_contrast_param_search(params)
+    run_simulation(params)
 
     #results, file_names = load_all(params)
     #plot.generate_memory_examples(results[0])
