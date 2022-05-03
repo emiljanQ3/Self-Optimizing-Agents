2927760adc0a7217b161a56865f1586d818c5838
Emil <emiljanQ3@hotmail.com>
2022-05-03 09:34:02 +0200

    UPDATE config for genetic multirun

--- config.py
+++ config.py
@@ -1,18 +1,21 @@
 from tags import WorldTag, MoveTag, AlphaInitTag
 from numpy import pi
 
+r0 = 2
+r1 = -6
+
 class Params:
     def __init__(self):
         # Meta
-        self.num_steps = 100000
-        self.num_repeats = 100
-        self.num_agents = 10
-        self.save_id = ""
-        self.results_path = "results/overview_-2_-6/"
+        self.num_steps = 10000
+        self.num_repeats = 1
+        self.num_agents = 100
+        self.save_id = f"genetic_run_{r0}_{r1}"
+        self.results_path = f"thesis_data/genetic_run_{r0}_{r1}/"
         self.alpha_tag = AlphaInitTag.NETWORK
-        self.model_location = "saved_genetic_training/genetic_run_6_4/genetic_run_6_4"
+        self.model_location = f"saved_genetic_training/genetic_run_{r0}_{r1}/genetic_run_{r0}_{r1}"
         # Learning
-        self.is_genetic_training = False
+        self.is_genetic_training = True
         self.is_backprop_training = False
         self.update_memory = True
         self.memory_length = 4
@@ -28,18 +31,18 @@ class Params:
         self.cell_size = 1
         self.area_unit_size = 0.05
         self.obstacle_size = 0.8  # relative to cell
-        self.tic_rate_0 = 6   # Resistance in even cells
-        self.tic_rate_1 = 4   # Resistance in odd cells
+        self.tic_rate_0 = r0   # Resistance in even cells
+        self.tic_rate_1 = r1   # Resistance in odd cells
         self.world_height = 2   # Currently only used for initializing the position of the agents
         self.world_width = 2   # Currently only used for initializing the position of the agents
         # Record
         self.is_recording_position = False
-        self.is_recording_area = False
+        self.is_recording_area = True
         self.is_recording_area_indices = False
-        self.is_recording_area_over_time = True
+        self.is_recording_area_over_time = False
         self.is_recording_loss = False
         self.is_recording_buffer_dataset = False
-        self.is_recording_distribution = True
+        self.is_recording_distribution = False
         # Plot
         self.is_plotting_trajectories = False
         self.is_plotting_area_units = False
