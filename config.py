18a70709af7fffb3f125b94a2dd978c02744acbe
Emil <emiljanQ3@hotmail.com>
2022-05-05 15:55:35 +0200

    UPDATE main and config for rerun of optimal local + static alpha strategies

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ r1 = -6
 class Params:
     def __init__(self):
         # Meta
-        self.num_steps = 10000
-        self.num_repeats = 1
-        self.num_agents = 100
-        self.save_id = f"genetic_run_{r0}_{r1}"
-        self.results_path = f"thesis_data/genetic_run_{r0}_{r1}/"
+        self.num_steps = 100000
+        self.num_repeats = 100
+        self.num_agents = 10
+        self.save_id = ""
+        self.results_path = f"thesis_data/overviews/overview_{r0}_{r1}/"
         self.alpha_tag = AlphaInitTag.NETWORK
         self.model_location = f"saved_genetic_training/genetic_run_{r0}_{r1}/genetic_run_{r0}_{r1}"
         # Learning
-        self.is_genetic_training = True
+        self.is_genetic_training = False
         self.is_backprop_training = False
         self.update_memory = True
         self.memory_length = 4
@@ -37,12 +37,12 @@ class Params:
         self.world_width = 2   # Currently only used for initializing the position of the agents
         # Record
         self.is_recording_position = False
-        self.is_recording_area = True
+        self.is_recording_area = False
         self.is_recording_area_indices = False
-        self.is_recording_area_over_time = False
+        self.is_recording_area_over_time = True
         self.is_recording_loss = False
         self.is_recording_buffer_dataset = False
-        self.is_recording_distribution = False
+        self.is_recording_distribution = True
         # Plot
         self.is_plotting_trajectories = False
         self.is_plotting_area_units = False
