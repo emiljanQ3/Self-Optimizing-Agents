176adbbd2eb335cdbbece16aecf5252f301591bc
Emil <emiljanQ3@hotmail.com>
2022-05-05 16:00:24 +0200

    UPDATE main and config for rerun of optimal local + static alpha strategies, additional fix

--- config.py
+++ config.py
@@ -12,19 +12,19 @@ class Params:
         self.num_agents = 10
         self.save_id = ""
         self.results_path = f"thesis_data/overviews/overview_{r0}_{r1}/"
-        self.alpha_tag = AlphaInitTag.NETWORK
+        self.alpha_tag = AlphaInitTag.SAME
         self.model_location = f"saved_genetic_training/genetic_run_{r0}_{r1}/genetic_run_{r0}_{r1}"
         # Learning
         self.is_genetic_training = False
         self.is_backprop_training = False
-        self.update_memory = True
+        self.update_memory = False
         self.memory_length = 4
         self.memory_compression_factor = 64
         # Movement
         self.delta_time = 0.5
         self.speed = 0.01
         self.ang_sd = pi/6
-        self.selected_mover = MoveTag.DIRECT_TIMER
+        self.selected_mover = MoveTag.LEVY_VARYING_DELTA_CONTRAST
         self.alpha = 1  # Only relevant if the current selected strategy does not change alpha
         # World
         self.selected_world = WorldTag.CONCAVE_CELLS
@@ -42,7 +42,7 @@ class Params:
         self.is_recording_area_over_time = True
         self.is_recording_loss = False
         self.is_recording_buffer_dataset = False
-        self.is_recording_distribution = True
+        self.is_recording_distribution = False
         # Plot
         self.is_plotting_trajectories = False
         self.is_plotting_area_units = False
