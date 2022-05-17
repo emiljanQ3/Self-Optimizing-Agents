eac1b0ba4bb4ae77b1c1a4c322c078a8762cce15
Emil <emiljanQ3@hotmail.com>
2022-05-17 07:06:33 +0200

    UPDATE main to read variables correctly

--- main.py
+++ main.py
@@ -60,11 +60,11 @@ def run_contrast_param_search(params: Params):
 
 
 def run_alpha_linspace(params:Params):
-    if sys.argv[1] == 0:
+    if sys.argv[1] == "0":
         params.selected_world = tags.WorldTag.CONCAVE_CELLS
-    elif sys.argv[1] == 1:
+    elif sys.argv[1] == "1":
         params.selected_world = tags.WorldTag.CONVEX_CELLS
-    elif sys.argv[1] == 2:
+    elif sys.argv[1] == "2":
         params.selected_world = tags.WorldTag.EMPTY
     else:
         print("invalid job argument")
